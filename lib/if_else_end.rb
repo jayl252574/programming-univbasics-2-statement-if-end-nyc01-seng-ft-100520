2edc34ebe5436e1b5ea5994d7889d57f0ec4154e
Jay Liu <jayl252574@gmail.com>
2020-09-18 14:28:21 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,5 @@
-if(condition1)
-//code to be executed if condition1is true.
-elsif (condition2)
-//code to be executed if condition2 is true.
-else (condition3)
-//code to be executed if condition3 is true.
-end.
+if true
+  puts "Even!"
+else
+  puts "Odd!"
+end
