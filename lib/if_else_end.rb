cca631f4346f144cf638f3ff509727a5d64d512a
Jay Liu <jayl252574@gmail.com>
2020-09-18 14:25:44 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,7 @@
-# Write your solution here
+if(condition1)
+//code to be executed if condition1is true.
+elsif (condition2)
+//code to be executed if condition2 is true.
+else (condition3)
+//code to be executed if condition3 is true.
+end.
